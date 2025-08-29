7d7b55217ceafaafccbf87c8f2386d09831f6ee3
Divyanshu Verma <divyanshuj22@gmail.com>
2025-08-29 22:57:52 +0530

    registration, otp verification and login feature completed

--- Server/controller/userAuth.js
+++ Server/controller/userAuth.js
@@ -2,6 +2,8 @@
     const jwt = require('jsonwebtoken')
     const bcrypt = require("bcrypt");
     const generateVerificationCode= require("../utils/verificationCodeGenerator")
+    const generateEmailTemplate= require("../utils/emailTemplate")
+    const sendEmail= require('../utils/sendEmail')
     const register=async (req,res)=>{
         try{
             let {name,email,password,phone,verificationMethod}= req.body;
@@ -23,47 +25,42 @@
                 throw new Error("User already exists")
             }
             const user = await User.findOne({
-        $or: [
-            { phone, accountVerified: false },
-            { email, accountVerified: false },
-        ],
-        });
-        console.log("completed  till this ")
-        if (user && user.accountVerification.length > 3) {
-        throw new Error("You have exceeded the maximum number of attempts (3). Please try again after an hour.");
+                $or: [
+                    { phone, accountVerified: false },
+                    { email, accountVerified: false },
+                ],
+            });
+            console.log("completed  till this ")
+            if (user && user.accountVerification.length > 3) {
+                throw new Error("You have exceeded the maximum number of attempts (3). Please try again after an hour.");
+            }
+            const userData = {
+                name,email,phone ,password,
+                accountVerified:false
+            }
+            let verificationObj={};
+            console.log(user);
+            if (!user){
+                // genrate code 
+                const verificationCode= generateVerificationCode();
+                verificationObj.verificationMethod= verificationMethod;
+                verificationObj.verificationCode = verificationCode;
+                userData.accountVerification= [verificationObj];
+                const userInfo= await User.create(userData);
+                console.log("checking verificaton method", verificationMethod)
+                sendVerificationCode(  verificationMethod,  verificationCode,  userInfo.name,  userInfo.email,  userInfo.phone,  res)
         }
-        const userData = {
-            name,email,phone ,password,
-            accountVerified:false
-        }
-        let verificationObj={};
-        console.log(user);
-        if (!user){
-            // genrate code 
-            const verificationCode= generateVerificationCode();
-            verificationObj.verificationMethod= verificationMethod;
-            verificationObj.verificationCode = verificationCode;
-            userData.accountVerification= [verificationObj];
-            const userInfo= await User.create(userData);
-            res.status(200).json({
-                message:"user resgister successfully",
-                userInfo,
-            })
+            else if (user.accountVerification.length<=3){
+                const verificationCode= generateVerificationCode();
+                verificationObj.verificationMethod= verificationMethod;
+                verificationObj.verificationCode = verificationCode;
 
-
-        }
-        else if (user.accountVerification.length<=3){
-            const verificationCode= generateVerificationCode();
-            verificationObj.verificationMethod= verificationMethod;
-            verificationObj.verificationCode = verificationCode;
-
-            user.accountVerification= [verificationObj,...user.accountVerification];
-            const userInfo =await user.save();
-            res.status(200).json({
-                message:"user register successfully",
-                userInfo,
-            })
-        }
+                user.accountVerification= [verificationObj,...user.accountVerification];
+                const userInfo =await user.save();
+                console.log("checking verificaton method", verificationMethod)
+                sendVerificationCode(  verificationMethod,  verificationCode,  userInfo.name,  userInfo.email,  userInfo.phone,  res)
+            }
+        
             
         }
         catch(err){
@@ -73,6 +70,16 @@
     const verifyOtp = async (req,res)=>{
         const { email, otp, phone } = req.body;
         try{
+            const userCheck= await User.findOne({
+                accountVerified:true,
+                 $or: [
+                      { email },
+                      { phone }]
+                      })
+            console.log(userCheck);
+            if (userCheck ){
+                throw new Error("user already verified");
+            }
             const userData= await User.findOne({
                 accountVerified:false,
                  $or: [
@@ -85,16 +92,23 @@
              }
              let user ;
              if (userData.accountVerification.length>1){
+                console.log("userData",userData)
                  user = userData.accountVerification[0]; 
                  userData.accountVerification=[user];
              }
-             else {
+             else if (userData.accountVerification.length==1) {
+                 console.log("userData2",userData)
                  user = userData.accountVerification[0];
 
             }
+            else if (userData.accountVerification.length==0){
+                throw new Error("verification code not found")
+            }
+            console.log(user);
              if (user.verificationCode != Number(otp)){
                   throw new Error("invaild otp ")
         }
+        console.log("otp verification done ")
         const currentTime= Date.now()
         const verificationCodeExpire= new Date(userData.accountVerification.verificationCodeExpire).getTime()
         if (currentTime > verificationCodeExpire) {
@@ -123,5 +137,81 @@
 
     }
 
+    
+async function sendVerificationCode(  verificationMethod,  verificationCode,  name,  email,  phone,  res) {
+  try {
+    console.log(verificationMethod)
+    if (verificationMethod === "email") {
+      const message = generateEmailTemplate(verificationCode);
+      console.log(message)
+      sendEmail({ email, subject: "Your Verification Code", message });
+      res.status(200).json({
+        success: true,
+        message: `Verification email successfully sent to ${name}`,
+      });
+      console.log("messasge send ")
+    } else if (verificationMethod === "phone") {
+      const verificationCodeWithSpace = verificationCode
+        .toString()
+        .split("")
+        .join(" ");
+      await client.calls.create({
+        twiml: `<Response><Say>Your verification code is ${verificationCodeWithSpace}. Your verification code is ${verificationCodeWithSpace}.</Say></Response>`,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: phone,
+      });
+      res.status(200).json({
+        success: true,
+        message: `OTP sent.`,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        message: "Invalid verification method.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Verification code failed to send.",
+    });
+  }
+}
+const login = async (req, res) => {
+    try{
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Email and password are required.", 400));
+  }
+  const user = await User.findOne({ email, accountVerified: true }).select(
+    "+password"
+  );
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password.", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password.", 400));
+  }
+    const token = jwt.sign({_id:userData._id, emailId:userData.email},"asffds",{expiresIn:3600});
+    res.cookie('token',token,{maxAge:60*60*1000});
+    const reply={
+            name:userData.name,
+            emailId:userData.email,
+            _id:userData._id
+        }
+        res.status(201).json({
+            user:reply,
+            message:"otp is verified !" 
+        })
+}catch(err){
+    res.status(500).json({
+        error:err.message
+    })
+}
+};
 
-    module.exports={register,verifyOtp}
+
+
+    module.exports={register,verifyOtp,login}
