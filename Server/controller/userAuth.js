ac3ae295d3dee06b723e6d265fdc3de38b411060
Divyanshu Verma <divyanshuj22@gmail.com>
2025-10-12 15:50:11 +0530

    properly configure secure cookies, sameSite flags, and CORS for seamless cross-domain authentication between Render and Vercel

--- Server/controller/userAuth.js
+++ Server/controller/userAuth.js
@@ -1,161 +1,166 @@
-    const User= require("../model/user")
-    const jwt = require('jsonwebtoken')
-    const crypto = require("crypto");
+const User = require("../model/user")
+const jwt = require('jsonwebtoken')
+const crypto = require("crypto");
 
-    const bcrypt = require("bcrypt");
-    const generateVerificationCode= require("../utils/verificationCodeGenerator")
-    const generateEmailTemplate= require("../utils/emailTemplate")
-    const generateResetPasswordToken= require('../utils/generateResetToken')
-    const  twilio = require("twilio");
+const bcrypt = require("bcrypt");
+const generateVerificationCode = require("../utils/verificationCodeGenerator")
+const generateEmailTemplate = require("../utils/emailTemplate")
+const generateResetPasswordToken = require('../utils/generateResetToken')
+const twilio = require("twilio");
 
-    const sendEmail= require('../utils/sendEmail')
-    
-    const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+const sendEmail = require('../utils/sendEmail')
 
-    const register=async (req,res)=>{
-        console.log("register is called ")
-        try{
-            console.log("req.body",req.body)
-            let {name,email,password,phone,verificationMethod}= req.body;
-            console.log("name",name)
-            password = await bcrypt.hash(password,10);
-            // checking for existing user 
-            const existingUser= await User.findOne({
-                $or:[
-                    {
-                        email,
-                        accountVerified:true,
-                    },
-                    {
-                        phone,
-                        accountVerified:true,
-                    }
-                ]
-            })
-            if (existingUser){
-                throw new Error("User already exists")
-            }
-            const user = await User.findOne({
-                $or: [
-                    { phone, accountVerified: false },
-                    { email, accountVerified: false },
-                ],
-            });
-            console.log("completed  till this ")
-            if (user && user.accountVerification.length > 3) {
-                throw new Error("You have exceeded the maximum number of attempts (3). Please try again after an hour.");
-            }
-            const userData = {
-                name,email,phone ,password,
-                accountVerified:false
-            }
-            let verificationObj={};
-            console.log(user);
-            if (!user){
-                // genrate code 
-                const verificationCode= generateVerificationCode();
-                verificationObj.verificationMethod= verificationMethod;
-                verificationObj.verificationCode = verificationCode;
-                userData.accountVerification= [verificationObj];
-                const userInfo= await User.create(userData);
-                console.log("checking verificaton method", verificationMethod)
-                console.log("user name :", userInfo.name)
-                sendVerificationCode(  verificationMethod,  verificationCode,  userInfo.name,  userInfo.email,  userInfo.phone,  res)
+const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+const register = async (req, res) => {
+  console.log("register is called ")
+  try {
+    console.log("req.body", req.body)
+    let { name, email, password, phone, verificationMethod } = req.body;
+    console.log("name", name)
+    password = await bcrypt.hash(password, 10);
+    // checking for existing user 
+    const existingUser = await User.findOne({
+      $or: [
+        {
+          email,
+          accountVerified: true,
+        },
+        {
+          phone,
+          accountVerified: true,
         }
-            else if (user.accountVerification.length<=3){
-                const verificationCode= generateVerificationCode();
-                verificationObj.verificationMethod= verificationMethod;
-                verificationObj.verificationCode = verificationCode;
-
-                user.accountVerification= [verificationObj,...user.accountVerification];
-                const userInfo =await user.save();
-                console.log("checking verificaton method", verificationMethod)
-                                console.log("user name :", userInfo.name)
-
-                sendVerificationCode(  verificationMethod,  verificationCode,  userInfo.name,  userInfo.email,  userInfo.phone,  res)
-            }
-        
-            
-        }
-        catch(err){
-        return res.status(400).json({ error: err.message });
-        }
+      ]
+    })
+    if (existingUser) {
+      throw new Error("User already exists")
     }
-    const verifyOtp = async (req,res)=>{
-        const { email, otp, phone } = req.body;
-        try{
-            const userCheck= await User.findOne({
-                accountVerified:true,
-                 $or: [
-                      { email },
-                      { phone }]
-                      })
-            console.log(userCheck);
-            if (userCheck ){
-                throw new Error("user already verified");
-            }
-            const userData= await User.findOne({
-                accountVerified:false,
-                 $or: [
-                      { email },
-                      { phone }]
-                      })
-            console.log(userData)
-            if (!userData){
-            throw new Error("User not found")
-             }
-             let user ;
-             if (userData.accountVerification.length>1){
-                console.log("userData",userData)
-                 user = userData.accountVerification[0]; 
-                 userData.accountVerification=[user];
-             }
-             else if (userData.accountVerification.length==1) {
-                 console.log("userData2",userData)
-                 user = userData.accountVerification[0];
+    const user = await User.findOne({
+      $or: [
+        { phone, accountVerified: false },
+        { email, accountVerified: false },
+      ],
+    });
+    console.log("completed  till this ")
+    if (user && user.accountVerification.length > 3) {
+      throw new Error("You have exceeded the maximum number of attempts (3). Please try again after an hour.");
+    }
+    const userData = {
+      name, email, phone, password,
+      accountVerified: false
+    }
+    let verificationObj = {};
+    console.log(user);
+    if (!user) {
+      // genrate code 
+      const verificationCode = generateVerificationCode();
+      verificationObj.verificationMethod = verificationMethod;
+      verificationObj.verificationCode = verificationCode;
+      userData.accountVerification = [verificationObj];
+      const userInfo = await User.create(userData);
+      console.log("checking verificaton method", verificationMethod)
+      console.log("user name :", userInfo.name)
+      sendVerificationCode(verificationMethod, verificationCode, userInfo.name, userInfo.email, userInfo.phone, res)
+    }
+    else if (user.accountVerification.length <= 3) {
+      const verificationCode = generateVerificationCode();
+      verificationObj.verificationMethod = verificationMethod;
+      verificationObj.verificationCode = verificationCode;
 
-            }
-            else if (userData.accountVerification.length==0){
-                throw new Error("verification code not found")
-            }
-            console.log(user);
-             if (user.verificationCode != Number(otp)){
-                  throw new Error("invaild otp ")
-        }
-        console.log("otp verification done ")
-        const currentTime= Date.now()
-        const verificationCodeExpire= new Date(userData.accountVerification.verificationCodeExpire).getTime()
-        if (currentTime > verificationCodeExpire) {
-        throw new Error("OTP Expired.");
-        }
-        userData.accountVerified= true;
-        userData.accountVerification=[];
+      user.accountVerification = [verificationObj, ...user.accountVerification];
+      const userInfo = await user.save();
+      console.log("checking verificaton method", verificationMethod)
+      console.log("user name :", userInfo.name)
 
-        await userData.save();
-        const token = jwt.sign({_id:userData._id, emailId:userData.email},process.env.JWT_KEY,{expiresIn:3600});
-        res.cookie('token',token,{maxAge:60*60*1000});
-        const reply={
-            name:userData.name,
-            emailId:userData.email,
-            _id:userData._id
-        }
-        res.status(201).json({
-            user:reply,
-            message:"otp is verified !" 
-        })
-        
-        }
-        catch(err){
-            res.status(401).send("Error: "+err);
-        }
-
+      sendVerificationCode(verificationMethod, verificationCode, userInfo.name, userInfo.email, userInfo.phone, res)
     }
 
-    
-async function sendVerificationCode(  verificationMethod,  verificationCode,  name,  email,  phone,  res) {
+
+  }
+  catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+}
+const verifyOtp = async (req, res) => {
+  const { email, otp, phone } = req.body;
+  try {
+    const userCheck = await User.findOne({
+      accountVerified: true,
+      $or: [
+        { email },
+        { phone }]
+    })
+    console.log(userCheck);
+    if (userCheck) {
+      throw new Error("user already verified");
+    }
+    const userData = await User.findOne({
+      accountVerified: false,
+      $or: [
+        { email },
+        { phone }]
+    })
+    console.log(userData)
+    if (!userData) {
+      throw new Error("User not found")
+    }
+    let user;
+    if (userData.accountVerification.length > 1) {
+      console.log("userData", userData)
+      user = userData.accountVerification[0];
+      userData.accountVerification = [user];
+    }
+    else if (userData.accountVerification.length == 1) {
+      console.log("userData2", userData)
+      user = userData.accountVerification[0];
+
+    }
+    else if (userData.accountVerification.length == 0) {
+      throw new Error("verification code not found")
+    }
+    console.log(user);
+    if (user.verificationCode != Number(otp)) {
+      throw new Error("invaild otp ")
+    }
+    console.log("otp verification done ")
+    const currentTime = Date.now()
+    const verificationCodeExpire = new Date(userData.accountVerification.verificationCodeExpire).getTime()
+    if (currentTime > verificationCodeExpire) {
+      throw new Error("OTP Expired.");
+    }
+    userData.accountVerified = true;
+    userData.accountVerification = [];
+
+    await userData.save();
+    const token = jwt.sign({ _id: userData._id, emailId: userData.email }, process.env.JWT_KEY, { expiresIn: 3600 });
+    res.cookie('token', token, {
+      maxAge: 60 * 60 * 1000,        // 1 hour
+      httpOnly: true,                 // prevent JS access
+      secure: true,                   // required for 'none'
+      sameSite: 'none',               // allow cross-domain
+    });
+    const reply = {
+      name: userData.name,
+      emailId: userData.email,
+      _id: userData._id
+    }
+    res.status(201).json({
+      user: reply,
+      message: "otp is verified !"
+    })
+
+  }
+  catch (err) {
+    res.status(401).send("Error: " + err);
+  }
+
+}
+
+
+async function sendVerificationCode(verificationMethod, verificationCode, name, email, phone, res) {
   try {
     console.log("this is user name ", name)
-    console.log("sending verification code ",verificationMethod , verificationCode , name , email , phone )
+    console.log("sending verification code ", verificationMethod, verificationCode, name, email, phone)
     console.log(verificationMethod)
     if (verificationMethod === "email") {
       const message = generateEmailTemplate(verificationCode);
@@ -195,63 +200,68 @@ async function sendVerificationCode(  verificationMethod,  verificationCode,  na
   }
 }
 const login = async (req, res) => {
-    try{
-      console.log("login api started , req.body:",req.body)
-        const {email, password}=req.body;
-        if(!email){
-            throw new Error("Invalid Credential email !")
-        }
-        if(!password){
-            throw new Error("Invaild Credentail passworrd !")
-        }
-      const userData = await User.findOne({ email, accountVerified: true }).select(
-        "+password"
-      );
-      if (!userData) {
-         throw new Error("Invalid email or password.");
-      }
-      const match = await bcrypt.compare(password,userData.password)
-        if (!match){
-            throw new Error("Invaild Credential not matched!")
-        }
-      const token = jwt.sign({_id:userData._id, emailId:userData.email},process.env.JWT_KEY,{expiresIn:3600});
-        res.cookie('token',token,{maxAge:60*60*1000,  secure: true , httpOnly: true,           // secure: JS can't access it
-  sameSite: 'none',           });
-      const reply={
-            name:userData.name,
-            email:userData.email,
-            _id:userData._id
-        }
-      res.status(201).json({
-            user:reply,
-            message:"login successfully" 
-        })
-      }catch(err){
-        console.log("Error in login :",err);
-        res.status(500).json({
-        error:err.message
-    })
+  try {
+    console.log("login api started , req.body:", req.body)
+    const { email, password } = req.body;
+    if (!email) {
+      throw new Error("Invalid Credential email !")
     }
-  };
-const logout = async (req,res)=>{
-  try{
-  res.status(200).cookie("token","",{
-    expires:new Date(Date.now()),
-    httpOnly:true,
-  }).json({
-    success:true,
-    message:"logged out successfully "
-  })
-  }catch(err){
-  res.status(500).json({
-    success:false,
-    message:"fail to logout"
-  })
+    if (!password) {
+      throw new Error("Invaild Credentail passworrd !")
+    }
+    const userData = await User.findOne({ email, accountVerified: true }).select(
+      "+password"
+    );
+    if (!userData) {
+      throw new Error("Invalid email or password.");
+    }
+    const match = await bcrypt.compare(password, userData.password)
+    if (!match) {
+      throw new Error("Invaild Credential not matched!")
+    }
+    const token = jwt.sign({ _id: userData._id, emailId: userData.email }, process.env.JWT_KEY, { expiresIn: 3600 });
+    res.cookie('token', token, {
+      maxAge: 60 * 60 * 1000, secure: true, httpOnly: true,           // secure: JS can't access it
+      sameSite: 'none',
+    });
+    const reply = {
+      name: userData.name,
+      email: userData.email,
+      _id: userData._id
+    }
+    res.status(201).json({
+      user: reply,
+      message: "login successfully"
+    })
+  } catch (err) {
+    console.log("Error in login :", err);
+    res.status(500).json({
+      error: err.message
+    })
+  }
+};
+const logout = async (req, res) => {
+  try {
+    res.status(200).cookie("token", "", {
+      expires: new Date(Date.now()),  // expire immediately
+      httpOnly: true,                 // prevent JS access
+      secure: true,                   // same as login
+      sameSite: "none",               // same as login
+    }).json({
+      success: true,
+      message: "Logged out successfully"
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "fail to logout"
+    })
   }
 }
 
- const forgotPassword = async (req, res) => {
-console.log("forgot password api called")
+const forgotPassword = async (req, res) => {
+  console.log("forgot password api called")
   const user = await User.findOne({
     email: req.body.email,
     accountVerified: true,
@@ -263,8 +273,8 @@ console.log("forgot password api called")
     throw new Error("User not found.");
   }
   const resetToken = generateResetPasswordToken();
-  user.resetPassword.resetPasswordExpire= Date.now() + 15 * 60 * 1000;
-  user.resetPassword.resetPasswordToken=resetToken
+  user.resetPassword.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+  user.resetPassword.resetPasswordToken = resetToken
   await user.save({ validateBeforeSave: false });
   const resetPasswordUrl = `${process.env.FRONTEND_URL}/resetPassword/${resetToken}`;
 
@@ -285,52 +295,52 @@ console.log("forgot password api called")
     user.resetPassword.resetPasswordExpire = undefined;
     await user.save({ validateBeforeSave: false });
     res.status(400).json({
-        error:error.message ? error.message : "Cannot send reset password token."
+      error: error.message ? error.message : "Cannot send reset password token."
     }
-      )
-    ;
+    )
+      ;
   }
 };
 const resetPassword = async (req, res) => {
-  try{
-  const { token } = req.params;
-  console.log("this reset password")
-  const resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(token)
-    .digest("hex");
+  try {
+    const { token } = req.params;
+    console.log("this reset password")
+    const resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(token)
+      .digest("hex");
     console.log("done")
-//   const user = await User.findOne({
-//   "resetPassword.resetPasswordToken": resetPasswordToken,
-//   "resetPassword.resetPasswordExpire": { $gt: Date.now() },
-// });
-const user = await User.findOne()
-console.log("user mil gya :",user)
+    //   const user = await User.findOne({
+    //   "resetPassword.resetPasswordToken": resetPasswordToken,
+    //   "resetPassword.resetPasswordExpire": { $gt: Date.now() },
+    // });
+    const user = await User.findOne()
+    console.log("user mil gya :", user)
 
-  if (!user) {
-    throw new Error("Reset password token is invalid or has been expired.")
+    if (!user) {
+      throw new Error("Reset password token is invalid or has been expired.")
+    }
+    console.log("user mil gya ");
+    if (req.body.password !== req.body.confirmPassword) {
+      throw new Error("Password & confirm password do not match.");
+    }
+    let password = req.body.password;
+    password = await bcrypt.hash(password, 10);
+    user.password = password;
+    user.resetPassword.resetPasswordToken = undefined;
+    user.resetPassword.resetPasswordExpire = undefined;
+    await user.save();
+    res.status(200).json({
+      message: "password updated successfully"
+    })
   }
-  console.log("user mil gya ");
-  if (req.body.password !== req.body.confirmPassword) {
-    throw new Error("Password & confirm password do not match.");
+  catch (err) {
+    res.status(400).json({
+      error: err
+    })
   }
-  let password= req.body.password;
-  password = await bcrypt.hash(password,10);
-  user.password = password;
-  user.resetPassword.resetPasswordToken = undefined;
-  user.resetPassword.resetPasswordExpire = undefined;
-  await user.save();
-  res.status(200).json({
-    message:"password updated successfully"
-  })
-}
-catch(err){
-  res.status(400).json({
-    error:err
-  })
-}
-  
+
 };
 
 
-module.exports={register,verifyOtp,login,logout,forgotPassword,resetPassword}
+module.exports = { register, verifyOtp, login, logout, forgotPassword, resetPassword }
--- Server/controller/userGoogleAuth.js
+++ Server/controller/userGoogleAuth.js
@@ -1,93 +1,105 @@
 
-const {oauth2client}= require('../utils/googleConfig')
+const { oauth2client } = require('../utils/googleConfig')
 const jwt = require("jsonwebtoken")
 const axios = require("axios");
 const User = require('../model/user');
-const googleLogin= async (req,res)=>{
-    try{
-      console.log("google login api started ")
-        const {code}=req.query;
-        console.log("code mil gya ",code)
-        const googleRes= await oauth2client.getToken(code);
-        // console.log("response bhi aa gaya ",googleRes)
-        oauth2client.setCredentials(googleRes.tokens);
-        const userRes= await axios.get(
-            `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
-        )
-        console.log("user ki detail bhi mil gyi ")
-        console.log(userRes.data);
-        const{email,name,picture}=userRes.data;
-        let userData= await User.findOne({email});
-        if (!userData) {
-         throw new Error("Email does not exists");
-      }
-        const token = jwt.sign({_id:userData._id, emailId:userData.email},process.env.JWT_KEY,{expiresIn:3600});
-        res.cookie('token',token,{maxAge:60*60*1000});
-      const reply={
-            name:userData.name,
-            email:userData.email,
-            _id:userData._id
-        }
-        console.log("google login api ne reponse bhi de diya ")
-      res.status(201).json({
-            user:reply,
-            message:"login successfully" 
-        }) 
-       }catch (err) {
-  console.error("Google OAuth Error:", err.response?.data || err.message);
+const googleLogin = async (req, res) => {
+  try {
+    console.log("google login api started ")
+    const { code } = req.query;
+    console.log("code mil gya ", code)
+    const googleRes = await oauth2client.getToken(code);
+    // console.log("response bhi aa gaya ",googleRes)
+    oauth2client.setCredentials(googleRes.tokens);
+    const userRes = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
+    )
+    console.log("user ki detail bhi mil gyi ")
+    console.log(userRes.data);
+    const { email, name, picture } = userRes.data;
+    let userData = await User.findOne({ email });
+    if (!userData) {
+      throw new Error("Email does not exists");
+    }
+    const token = jwt.sign({ _id: userData._id, emailId: userData.email }, process.env.JWT_KEY, { expiresIn: 3600 });
+    res.cookie('token', token, {
+      maxAge: 60 * 60 * 1000,        // 1 hour
+      httpOnly: true,                 // prevent JS access
+      secure: true,                   // required for 'none'
+      sameSite: 'none',               // allow cross-domain
+    });
 
-  return res.status(500).json({
-    message: "Internal server error",
-    error: err.message,
-    details: err.response?.data || null
-  });
-}
-}
+    const reply = {
+      name: userData.name,
+      email: userData.email,
+      _id: userData._id
+    }
+    console.log("google login api ne reponse bhi de diya ")
+    res.status(201).json({
+      user: reply,
+      message: "login successfully"
+    })
+  } catch (err) {
+    console.error("Google OAuth Error:", err.response?.data || err.message);
 
-const googleRegister= async (req,res)=>{
-    try{
-        const {code}=req.query;
-        console.log("code mil gya ",code)
-        const googleRes= await oauth2client.getToken(code);
-        // console.log("response bhi aa gaya ",googleRes)
-        oauth2client.setCredentials(googleRes.tokens);
-        const userRes= await axios.get(
-            `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
-        )
-        console.log("user ki detail bhi mil gyi ")
-        console.log(userRes.data);
-        const{email,name,picture}=userRes.data;
-        let userData= await User.findOne({email});
-        if (userData) {
-         throw new Error("user already exists!");
-      }
-      console.log("checked for user if it is exist")
-      userData= await User.create({
-        name:name,
-        email:email,
-        accountVerified:true
-      })
-      console.log("data entered in database",userData);
-        const token = jwt.sign({_id:userData._id, emailId:userData.email},process.env.JWT_KEY,{expiresIn:3600});
-        res.cookie('token',token,{maxAge:60*60*1000});
-      const reply={
-            name:userData.name,
-            email:userData.email,
-            _id:userData._id
-        }
-      res.status(201).json({
-            user:reply,
-            message:"login successfully" 
-        }) 
-       }catch (err) {
-  console.error("Google OAuth Error:", err.response?.data || err.message);
-
-  return res.status(500).json({
-    message: "Internal server error",
-    error: err.message,
-    details: err.response?.data || null
-  });
-}
+    return res.status(500).json({
+      message: "Internal server error",
+      error: err.message,
+      details: err.response?.data || null
+    });
+  }
 }
 
-module.exports= {googleLogin,googleRegister}
+const googleRegister = async (req, res) => {
+  try {
+    const { code } = req.query;
+    console.log("code mil gya ", code)
+    const googleRes = await oauth2client.getToken(code);
+    // console.log("response bhi aa gaya ",googleRes)
+    oauth2client.setCredentials(googleRes.tokens);
+    const userRes = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
+    )
+    console.log("user ki detail bhi mil gyi ")
+    console.log(userRes.data);
+    const { email, name, picture } = userRes.data;
+    let userData = await User.findOne({ email });
+    if (userData) {
+      throw new Error("user already exists!");
+    }
+    console.log("checked for user if it is exist")
+    userData = await User.create({
+      name: name,
+      email: email,
+      accountVerified: true
+    })
+    console.log("data entered in database", userData);
+    const token = jwt.sign({ _id: userData._id, emailId: userData.email }, process.env.JWT_KEY, { expiresIn: 3600 });
+    res.cookie('token', token, {
+      maxAge: 60 * 60 * 1000,        // 1 hour
+      httpOnly: true,                 // prevent JS access
+      secure: true,                   // required for 'none'
+      sameSite: 'none',               // allow cross-domain
+    });
+
+    const reply = {
+      name: userData.name,
+      email: userData.email,
+      _id: userData._id
+    }
+    res.status(201).json({
+      user: reply,
+      message: "login successfully"
+    })
+  } catch (err) {
+    console.error("Google OAuth Error:", err.response?.data || err.message);
+
+    return res.status(500).json({
+      message: "Internal server error",
+      error: err.message,
+      details: err.response?.data || null
+    });
+  }
+}
+
+module.exports = { googleLogin, googleRegister }
