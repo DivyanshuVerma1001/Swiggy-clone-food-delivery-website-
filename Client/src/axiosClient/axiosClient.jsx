0c5b237000b30dfcd6fab80fb5ed1390b1517a8a
Divyanshu Verma <divyanshuj22@gmail.com>
2025-10-31 21:38:15 +0530

    minor changes

--- Client/src/axiosClient/axiosClient.jsx
+++ Client/src/axiosClient/axiosClient.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosClient= axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://food-del-backend-cgd5.onrender.com",
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
